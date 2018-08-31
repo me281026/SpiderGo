ca8ddc375f0cefec4bd187c63893e484bd51213e
wanwei_zhou <wanwei_zhou@crfchina.com>
2018-08-31 09:46:44 +0800

    修改

--- me.spider.go/test/datasource/mysql/main.go
+++ me.spider.go/test/datasource/mysql/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"./connection"
+	"./user"
+	"fmt"
+)
 
+func main() {
+	var u user.User
+	err := connection.DBConnection.QueryRow("SELECT USERINFOID,USERID,PASSWORD,USERNAME"+
+		",SEX,AGE,BIRTHDAY,PHONENUM FROM crf_bi.bi_userinfo where USERINFOID = 1").Scan(u.USERINFOID, u.USERID, u.PASSWORD, u.USERNAME, u.SEX, u.AGE, u.BIRTHDAY, u.PHONENUM)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(u)
 }
