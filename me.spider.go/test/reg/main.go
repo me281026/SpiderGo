aa8f7a7ce60d106e9e68a88752ffa7bfc7490b7c
wanwei_zhou <wanwei_zhou@crfchina.com>
2018-09-06 17:06:06 +0800

    修改06

--- me.spider.go/test/reg/main.go
+++ me.spider.go/test/reg/main.go
@@ -4,27 +4,43 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"regexp"
 )
 
 func main() {
 	resp, err := http.Get("https://www.bilibili.com")
-	if err != nil {
-		fmt.Println("err:", err.Error())
-	}
+	DealErr(err)
 
 	defer resp.Body.Close()
 	all, err := ioutil.ReadAll(resp.Body)
 
 	match := regexp.MustCompile(`<a href="//(www.bilibili.com/[^>]*)><span>([^<]*)</span></a>`)
 	data := match.FindAllSubmatch(all, -1)
+
+	file, e := os.Open("D://GoProject//SpiderGo//me.spider.go//test//reg//HTML")
+	DealErr(e)
+	defer file.Close()
+
 	for _, v := range data {
+		for _, m := range v {
+			n, err2 := file.Write(m)
+			DealErr(err2)
+			fmt.Printf("写入 %d 个字节n", n)
+		}
+
 		fmt.Printf("area: %s , url: %s\n", v[2], v[1])
+
 	}
+	file.Sync()
 	fmt.Printf("total url num : %d\n", len(data))
 
+	DealErr(err)
+
+}
+
+func DealErr(err error) {
 	if err != nil {
 		fmt.Println("err:", err.Error())
 	}
-
 }
