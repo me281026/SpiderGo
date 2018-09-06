cae1a26416a4f3845aaf9417439e4f74ac76cb95
wanwei_zhou <wanwei_zhou@crfchina.com>
2018-09-06 14:02:06 +0800

    修改04

--- me.spider.go/test/reg/main.go
+++ me.spider.go/test/reg/main.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"regexp"
 )
 
 func main() {
-	resp, err := http.Get("http://bbs.nga.cn/index.php")
+	resp, err := http.Get("https://www.bilibili.com")
 	if err != nil {
 		fmt.Println("err:", err.Error())
 	}
@@ -15,9 +16,14 @@ func main() {
 	defer resp.Body.Close()
 	all, err := ioutil.ReadAll(resp.Body)
 
+	match := regexp.MustCompile(`<a href="//www.bilibili.com/.*</span></a>`)
+	data := match.FindAll(all, -1)
+	for _, v := range data {
+		fmt.Printf("%s\n", v)
+	}
+
 	if err != nil {
 		fmt.Println("err:", err.Error())
 	}
-	fmt.Printf("%s\n", all)
 
 }
