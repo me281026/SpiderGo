24ebc96c3caba94d96621033031a29d3ca1b2c4d
wanwei_zhou <wanwei_zhou@crfchina.com>
2018-09-18 16:09:11 +0800

    添加解答05

--- me.spider.go/test/leetcode/solition_01/problem02.go
+++ me.spider.go/test/leetcode/solition_01/problem02.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"sort"
 	"strings"
 )
 
@@ -114,4 +115,22 @@ func poorPigs(buckets int, minutesToDie int, minutesToTest int) int {
 // 前三名运动员将会被分别授予 “金牌”，“银牌” 和“ 铜牌”（"Gold Medal", "Silver Medal", "Bronze Medal"）。
 func findRelativeRanks(nums []int) []string {
 	return []string{}
+}
+
+//455. 分发饼干
+//假设你是一位很棒的家长，想要给你的孩子们一些小饼干。但是，每个孩子最多只能给一块饼干。
+// 对每个孩子 i ，都有一个胃口值 gi ，这是能让孩子们满足胃口的饼干的最小尺寸；
+// 并且每块饼干 j ，都有一个尺寸 sj 。如果 sj >= gi ，我们可以将这个饼干 j 分配给孩子 i ，这个孩子会得到满足。
+// 你的目标是尽可能满足越多数量的孩子，并输出这个最大数值。
+func findContentChildren(g []int, s []int) int {
+	sort.Ints(g)
+	sort.Ints(s)
+
+	num := 0
+	for i := 0; num < len(g) && i < len(s); i++ {
+		if s[i] >= g[i] {
+			num++
+		}
+	}
+	return num
 }
