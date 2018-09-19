9e7079077a52b0e6b8cb6cc70bd0083787b19a89
wanwei_zhou <wanwei_zhou@crfchina.com>
2018-09-19 10:08:15 +0800

    解答06

--- me.spider.go/test/leetcode/solition_01/problem02.go
+++ me.spider.go/test/leetcode/solition_01/problem02.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"regexp"
 	"sort"
 	"strings"
@@ -133,4 +134,31 @@ func findContentChildren(g []int, s []int) int {
 		}
 	}
 	return num
+}
+
+//453. 最小移动次数使数组元素相等
+//给定一个长度为 n 的非空整数数组，找到让数组所有元素相等的最小移动次数。每次移动可以使 n - 1 个元素增加 1。
+//输入:
+//[1,2,3]
+//
+//输出:
+//3
+//
+//解释:
+//只需要3次移动（注意每次移动会增加两个元素的值）：
+//
+//[1,2,3]  =>  [2,3,3]  =>  [3,4,3]  =>  [4,4,4]
+func minMoves(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	min := nums[0]
+	for _, v := range nums {
+		min = int(math.Min(float64(min), float64(v)))
+	}
+	result := 0
+	for _, v := range nums {
+		result += v - min
+	}
+	return result
 }
