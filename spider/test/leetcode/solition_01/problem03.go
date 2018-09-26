12e0d3196bee02cac41fdb8dcdcc348ffb4410dd
wanwei_zhou <wanwei_zhou@crfchina.com>
2018-09-26 17:36:20 +0800

    解答

--- spider/test/leetcode/solition_01/problem03.go
+++ spider/test/leetcode/solition_01/problem03.go
@@ -103,3 +103,16 @@ func smallestRangeI(A []int, K int) int {
 	return int(math.Max(0, max-min-2*float64(K)))
 
 }
+
+//342. 4的幂
+//给定一个整数 (32 位有符号整数)，请编写一个函数来判断它是否是 4 的幂次方。
+//输入: 16
+//输出: true
+//示例 2:
+//
+//输入: 5
+//输出: false
+func isPowerOfFour(num int) bool {
+	return num > 0 && num&(num-1) == 0 && num&0x55555555 == num
+
+}
