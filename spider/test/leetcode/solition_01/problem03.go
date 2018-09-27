3d3fe83461517c0d72715254a32712202d5a2d60
wanwei_zhou <wanwei_zhou@crfchina.com>
2018-09-27 13:46:57 +0800

    解答

--- spider/test/leetcode/solition_01/problem03.go
+++ spider/test/leetcode/solition_01/problem03.go
@@ -116,3 +116,24 @@ func isPowerOfFour(num int) bool {
 	return num > 0 && num&(num-1) == 0 && num&0x55555555 == num
 
 }
+
+//231. 2的幂
+//给定一个整数，编写一个函数来判断它是否是 2 的幂次方。
+//
+//示例 1:
+//
+//输入: 1
+//输出: true
+//解释: 20 = 1
+//示例 2:
+//
+//输入: 16
+//输出: true
+//解释: 24 = 16
+//示例 3:
+//
+//输入: 218
+//输出: false
+func isPowerOfTwo(n int) bool {
+	return n > 0 && (n&(n-1)) == 0
+}
