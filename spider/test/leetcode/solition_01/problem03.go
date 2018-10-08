f882a0bf65456079b997820bba1fe7e5552c8eb7
wanwei_zhou <wanwei_zhou@crfchina.com>
2018-10-08 14:39:45 +0800

    解答

--- spider/test/leetcode/solition_01/problem03.go
+++ spider/test/leetcode/solition_01/problem03.go
@@ -205,3 +205,40 @@ func isUgly02(num int) bool {
 	return num == 1
 
 }
+
+//541. 反转字符串 II
+//给定一个字符串和一个整数 k，你需要对从字符串开头算起的每个 2k 个字符的前k个字符进行反转。
+// 如果剩余少于 k 个字符，则将剩余的所有全部反转。如果有小于 2k 但大于或等于 k 个字符，则反转前 k 个字符，
+// 并将剩余的字符保持原样。
+//
+//示例:
+//
+//输入: s = "abcdefg", k = 2
+//输出: "bacdfeg"
+//要求:
+//
+//该字符串只包含小写的英文字母。
+//给定字符串的长度和 k 在[1, 10000]范围内。
+func reverseStr(s string, k int) string {
+	return ""
+}
+
+//405. 数字转换为十六进制数
+func toHex(num int) string {
+	var arr = [16]rune{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'}
+	if num == 0 {
+		return ""
+	}
+	var res string
+	for num != 0 {
+		res = string(arr[(num&15)]) + res
+
+	}
+	return ""
+
+}
+
+//888. 公平的糖果交换
+func fairCandySwap(A []int, B []int) []int {
+	return nil
+}
