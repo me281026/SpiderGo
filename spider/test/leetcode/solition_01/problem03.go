703c4b08d3cfbaed1d7588eea2bf8f1b7a34cd87
wanwei_zhou <wanwei_zhou@crfchina.com>
2018-10-29 16:54:17 +0800

    添加解答

--- spider/test/leetcode/solition_01/problem03.go
+++ spider/test/leetcode/solition_01/problem03.go
@@ -371,3 +371,21 @@ func isOneBitCharacter02(bits []int) bool {
 	}
 	return (len(bits)-num)%2 == 0
 }
+
+//66. 加一
+func plusOne(digits []int) []int {
+	var len int = len(digits)
+	for i := len - 1; i >= 0; i-- {
+		if digits[i] < 9 {
+			digits[i]++
+			return digits
+		}
+		digits[i] = 0
+	}
+	if digits[0] == 0 {
+		arr := make([]int, len+1)
+		arr[0] = 1
+		return arr
+	}
+	return digits
+}
