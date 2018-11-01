ce1829e939214ce25455c215afe4df294f5e5b6c
wanwei_zhou <wanwei_zhou@crfchina.com>
2018-11-01 10:36:46 +0800

    测试

---
+++ spider/job/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"./execute"
+	"./pipline"
+	"sync"
+)
+
+//定义变量
+var (
+	//设定抓取语言
+	lang = []string{
+		"golang",
+	}
+
+	//抓群的城市
+	citys = []string{
+		"北京",
+		"上海",
+		"深圳",
+		"杭州",
+		"广州",
+		"武汉",
+		"南京",
+		"成都",
+	}
+
+	//入参
+	InitParam = execute.InitParam{}
+
+	//出参
+	ResultParam = execute.ResultParam{}
+
+	//初始化接入数据
+	JobInfo = pipline.InitJobInfo()
+
+	//协程计数
+	wg sync.WaitGroup
+)
+
+func main() {
+
+	//依次传入参数
+	for kd := range lang {
+		for city := range citys {
+			//传入一个执行协程
+			wg.Add(1)
+			go func(kd string, city string) {
+				//进入协程后取出
+				wg.Done()
+				//初始化参数并传入
+
+			}()
+
+		}
+	}
+
+}
